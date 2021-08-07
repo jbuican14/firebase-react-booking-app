629e313c888f6772ecc137629a727b34a57d6ca1
Juti Buican <jbuican19@gmail.com>
2021-08-07 11:17:26 +0100

    Set up firebase config, install and log in

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
 import './index.css';
 import { App, serviceWorker } from './app';
 
 import 'react-dates/initialize';
 import 'react-dates/lib/css/_datepicker.css';
+
+// After installing firebase package
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// check firebase log in 'firebase login --reauth's
+var firebaseConfig = {
+  apiKey: 'AIzaSyAv_tCb9b4AW2WB3v9H0tVPlPGhcNLpQkk',
+  authDomain: 'restaurant-app-demo-77bba.firebaseapp.com',
+  projectId: 'restaurant-app-demo-77bba',
+  storageBucket: 'restaurant-app-demo-77bba.appspot.com',
+  messagingSenderId: '273295484065',
+  appId: '1:273295484065:web:7733a0d42b51ade9423ce0',
+  measurementId: 'G-F10JHLGL1B',
+};
 
 // This is where the magic happens. React renders our App component
 // inside the div with the id "root"
